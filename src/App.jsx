4dac0c68f3c2153c2bb8c90bb663a0283c15c8b0
loneship1234 <parvizazady@gmail.com>
2022-05-21 12:38:45 +0430

    v-1.1

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import CardSecondary from './components/CardSecondary'
 function App() {
   return (
   <React.Fragment>
-    {/* <Card game='cyberpunk' theme='night' btnColor='btn-error'  badgeColor='badge-error' category_1='shotter' category_2='adventure' isNew={true}  text='this game is very good please buy it and make me milionare'/> */}
-    <CardSecondary game='dooms eternal' text='this game is very good please buy it and make me milionare'/>
+    <Card game='cyberpunk' theme='night' btnColor='btn-error'  badgeColor='badge-error' category_1='shotter' category_2='adventure' isNew={true}  text='this game is very good please buy it and make me milionare'/>
+    {/* <CardSecondary game='dooms eternal' text='this game is very good please buy it and make me milionare'/> */}
     </React.Fragment>
   )
 }
